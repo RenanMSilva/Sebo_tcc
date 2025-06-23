d1a69106b75552ba14b121b5a920e0dbd465eaee
RenanMSilva <renan.13.long@gmail.com>
2025-06-23 20:07:08 -0300

    Adicionar arquivos de projeto.

---
+++ Sebo_tcc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sebo_tcc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
